cb5f37fca26c72682388a0c49916abdd044c1e85
Tursunali008 <153731500+Tursunali008@users.noreply.github.com>
2024-06-15 17:14:03 +0500

    hichnarsa qmadm

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:imtihon/screen/splash_screen/splash_screen.dart';
+import 'package:imtihon/screen/homePage/home_screen.dart';
 import 'package:imtihon/utils/extension/extension.dart';
 import 'package:imtihon/view_model/auth_view_model.dart';
 import 'package:provider/provider.dart';
@@ -26,10 +26,7 @@ class MyApp extends StatelessWidget {
     height = context.getHeight();
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
-
-
-
